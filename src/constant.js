da949450407d1725206d1823681286cd43dde687
Koshal765 <koshalpothare@gamil.com>
2025-11-14 18:27:04 +0530

    PNG

--- src/constant.js
+++ src/constant.js
@@ -32,7 +32,7 @@ import vercelLogo from './assets/tech_logo/vercel.png';
 // import postgreLogo from './assets/tech_logo/postgre.png';
 // import csharpLogo from './assets/tech_logo/csharp.png';
 
-import Appt1 from './assets/projects/Appt.png';
+import Appt1 from './assets/projects/Appt.PNG';
 import Shayari from './assets/Projects/ShayariSagar.png';
 import Curr from './assets/projects/CurrerncyConverter.png';
 
